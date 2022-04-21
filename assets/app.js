6d83e51c20836fe8dac77bbcdc894e1f084f0a86
afeldten <a.feldten@neusta.de>
2022-04-21 14:46:47 +0200

    add global.scss

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,16 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
