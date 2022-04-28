6f22e2fab5ae9a071fed16e06e4e92957bd44de6
afeldten <a.feldten@neusta.de>
2022-04-28 11:10:17 +0200

    removed jquery

--- assets/app.js
+++ assets/app.js
@@ -11,7 +11,6 @@ import './styles/app.scss';
 // start the Stimulus application
 import './bootstrap';
 
-const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
@@ -19,7 +18,3 @@ require('bootstrap');
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
-
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-});
