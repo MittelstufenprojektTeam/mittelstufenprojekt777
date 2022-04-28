8d60b2fca82e818ac0481961fb163cf2f7f896f6
Sebastian <79452976+Sebastian-me@users.noreply.github.com>
2022-04-28 10:57:50 +0200

    MIT777-46 | Login (#10)
    * MIT777-46 [TASK] | Creating Login
    
    * MIT777-46 [TASK] | Creating Login (working)
    
    * MIT777-46 [TASK] | Creating Login (working)
    
    * MIT777-46 [TASK] | rework roll changes
    
    * MIT777-46 [TASK] | fixed for linter
    
    * MIT777-46 [TASK] | updated fonts
    
    * MIT777-46 [TASK] | deleted unused generated file
    
    * Fix progress data type for user
    
    Co-authored-by: sschmitz <s.schmitz@neusta.de>
    Co-authored-by: pzielinski <pzielinski@neusta.de>

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route(path: '/', name: 'app_home')]
+    public function home(): Response
+    {
+        return new JsonResponse(['message' => 'successfully logged in!']);
+    }
+}
