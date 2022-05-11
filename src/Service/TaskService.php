71489d90d485e22965f91653d59b633477af5758
Sebastian <79452976+Sebastian-me@users.noreply.github.com>
2022-05-11 11:23:56 +0200

    60 [FEATURE] | added Free text component (#15)
    * MIT777-60 [TASK] | added Free text component
    
    * MIT777-60 [FEATURE] | fixed for linter
    
    * MIT777-57 [TASK] | fixed for linter #2
    
    Co-authored-by: sschmitz <s.schmitz@neusta.de>

--- src/Service/TaskService.php
+++ src/Service/TaskService.php
@@ -33,6 +33,12 @@ class TaskService
         $question1->addOption($option1);
         $question2->addOption($option2);
 
-        return [$question1, $question2];
+        $freeText = new Question();
+        $option = new Option();
+        $freeText->setPhrase('Das ist ein Freitext: bitte schreibe etwas über Züge');
+        $option->setText('züge sind toll');
+        $freeText->addOption($option);
+
+        return [$question1, $question2, $freeText];
     }
 }
